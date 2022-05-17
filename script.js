d343de28518209afd825285fcd9e65b008258339
pavanchambhare <pavanchambhare88@gmail.com>
2022-05-17 21:02:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function change1()
+{
+    document.getElementById('pic').src = '1.png';
+}
+function change2()
+{
+    document.getElementById('pic').src = '2.png';
+}
+function change3()
+{
+    document.getElementById('pic').src = '3.png';
+}
+function change4()
+{
+    document.getElementById('pic').src = '4.png';
+}
+function change5()
+{
+    document.getElementById('pic').src = '5.png';
+}
+
+{
+    document.getElementById('green').innerHTML = "Green";
+}
+
+{
+    document.getElementById('blue').innerHTML = "blue";
+}
+
+{
+    document.getElementById('darkblue').innerHTML = "darkblue";
+}
+
+{
+    document.getElementById('yellow').innerHTML = "yellow";
+}
+{
+    document.getElementById('orange').innerHTML = "orange";
+}
